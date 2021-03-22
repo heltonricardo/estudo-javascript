50d4971ea83b0f0f5c7999a845cfa70bd89c9558
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-03-22 11:23:41 -0300

    Create 002-variaveis.js

---
+++ files/002-variaveis.js
@@ -0,0 +1,13 @@
+// Esse é um comentário!
+
+console.log("Trabalhando com variáveis")
+
+const idade = 20
+
+console.log("idade")
+console.log(idade)
+console.log(idade + 7)
+
+const idadeSomada = idade + 7
+
+console.log(idadeSomada)
