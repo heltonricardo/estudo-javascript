2fa82839e908573025e8d558645eec7bcd241901
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-03-22 11:55:22 -0300

    Create 004-conversaoDeTipos.js

---
+++ files/004-conversaoDeTipos.js
@@ -0,0 +1,16 @@
+console.log("Conversão de Tipos")
+
+console.log("Ano: " + 2021)
+
+// Concatenação de valores
+console.log("20" + "21")
+
+// Conversão explícita
+console.log(parseInt("20") + parseInt("21"))
+
+// Conversão inplícita
+console.log("10" / "2")
+console.log("7" / "2")
+
+// NaN: Not a Number
+console.log("Helton" / "2")
