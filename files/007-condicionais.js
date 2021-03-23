0332516ce9510fe58d26c58b64b39c214c74313d
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-03-23 09:55:42 -0300

    Update 007-condicionais.js

--- files/007-condicionais.js
+++ files/007-condicionais.js
@@ -4,11 +4,17 @@ const cidades = new Array("Curitiba", "Salvador", "São Paulo", "Rio de Janeiro"
 console.log(cidades)
 
 const idadeComprador = 15;
+const compradorAcompanhado = true;
 
 if (idadeComprador >= 18) {
     console.log("Comprador maior de idade")
     cidades.splice(2, 1)
 } else {
-    console.log("Comprador não é maior de idade")
+    if (compradorAcompanhado) {
+        console.log("Comprador não é maior de idade mas está acompanhado")
+        cidades.splice(2, 1)
+    } else {
+        console.log("Comprador não é maior de idade e não está acompanhado")
+    }
 }
 console.log(cidades)
