59b53f872ceccadd63ee12db77311f6bfcd6362e
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-03-23 09:55:45 -0300

    Create 008-operadoresLogicos.js

---
+++ files/008-operadoresLogicos.js
@@ -0,0 +1,16 @@
+console.log("Trabalhando com Condicionais")
+
+const cidades = new Array("Curitiba", "Salvador", "São Paulo", "Rio de Janeiro");
+console.log(cidades)
+
+const idadeComprador = 15;
+const compradorAcompanhado = false;
+
+if (idadeComprador >= 18 || compradorAcompanhado) {
+    console.log("Boa viagem!")
+    cidades.splice(2, 1)
+} else {
+    console.log("Comprador não é maior de idade e não está acompanhado")
+}
+
+console.log(cidades)
