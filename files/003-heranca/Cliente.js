4841248c255b04d34a2f16629a3575541db26caf
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-04-05 13:14:57 -0300

    Update Cliente.js

--- files/003-heranca/Cliente.js
+++ files/003-heranca/Cliente.js
@@ -1,13 +1,16 @@
 export class Cliente {
-    nome
-    _cpf
 
-    constructor(nome, cpf) {
+    constructor(nome, cpf, senha) {
         this.nome = nome
         this._cpf = cpf
+        this._senha = senha
     }
 
     get cpf() {
         return this._cpf
+    }
+
+    autenticar(senha) {
+        return true
     }
 }
