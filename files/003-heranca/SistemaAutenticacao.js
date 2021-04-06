e120d08c8f321cc0fda65da2ceb8077f64731a94
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-04-06 10:06:56 -0300

    Update SistemaAutenticacao.js

--- files/003-heranca/SistemaAutenticacao.js
+++ files/003-heranca/SistemaAutenticacao.js
@@ -1,7 +1,16 @@
 // Ser autenticável significa ter o método autenticar
 
 export class SistemaAutenticacao {
+    
     static login(autenticavel, senha) {
-        return autenticavel.autenticar(senha)
+        if (SistemaAutenticacao.isAutenticavel(autenticavel))
+            return autenticavel.autenticar(senha)
+        return false;
     }
+
+    static isAutenticavel(autenticavel) {
+        return "autenticar" in autenticavel
+            && autenticavel.autenticar instanceof Function
+    }
+
 }
