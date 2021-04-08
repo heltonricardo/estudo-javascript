112d22c1e1c624f6bc63dba0f8bef01fb98aac57
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-04-08 13:09:25 -0300

    Update 008-operadoresLogicos.js

--- files/001-conceitos/008-operadoresLogicos.js
+++ files/001-conceitos/008-operadoresLogicos.js
@@ -1,10 +1,10 @@
 console.log("Trabalhando com Operadores Lógicos")
 
-const cidades = new Array("Curitiba", "Salvador", "São Paulo", "Rio de Janeiro");
+const cidades = new Array("Curitiba", "Salvador", "São Paulo", "Rio de Janeiro")
 console.log(cidades)
 
-const idadeComprador = 15;
-const compradorAcompanhado = false;
+const idadeComprador = 15
+const compradorAcompanhado = false
 
 if (idadeComprador >= 18 || compradorAcompanhado) {
     console.log("Boa viagem!")
