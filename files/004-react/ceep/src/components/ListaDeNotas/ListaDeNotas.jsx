5ef54496c7b79953d8c8b310bf71f68e1d5e7715
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-04-09 14:02:13 -0300

    Update ListaDeNotas.jsx

--- files/004-react/ceep/src/components/ListaDeNotas/ListaDeNotas.jsx
+++ files/004-react/ceep/src/components/ListaDeNotas/ListaDeNotas.jsx
@@ -13,10 +13,10 @@ class ListaDeNotas extends Component {
   render() {
     return (
       <ul className="lista-notas">
-        {Array.of("Trabalho", "Trabalho", "Estudos").map((categoria, index) => {
+        {this.props.notas.map((nota, index) => {
           return (
             <li className="lista-notas_item" key={index}>
-              <CardNota />
+              <CardNota titulo={nota.titulo} texto={nota.texto}/>
             </li>
           )
         })}
