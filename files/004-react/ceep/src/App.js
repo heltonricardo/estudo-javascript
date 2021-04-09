4508e3293f0e3015156b24f3abea89d1a49f62f3
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-04-09 14:01:59 -0300

    Update App.js

--- files/004-react/ceep/src/App.js
+++ files/004-react/ceep/src/App.js
@@ -5,11 +5,27 @@ import "./assets/App.css"
 import './assets/index.css'
 
 class App extends Component {
+constructor() {
+  super()
+  this.notas = []
+
+  // Propriedade especial para renderizar novamente a página, quando ele é \
+  // atualizado, o método render é automaticamente invocado
+  this.state = { notas:[] }
+}
+
+  criacaoDeNota(titulo, texto) {
+    const novaNota = {titulo, texto}
+    const novoArrayNotas = [...this.state.notas, novaNota]
+    const novoEstado = { notas:novoArrayNotas }
+    this.setState(novoEstado)
+  }
+
   render() {
     return (
       <section className="conteudo">
-        <FormularioCadastro />
-        <ListaDeNotas />
+        <FormularioCadastro criarNota={this.criacaoDeNota.bind(this)}/>
+        <ListaDeNotas notas={this.state.notas}/>
       </section>
     )
   }
